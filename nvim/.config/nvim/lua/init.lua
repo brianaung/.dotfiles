fb433e28c80ec66ae1fd370b6608102756936057
Brian Aung <soekhantaung16@gmail.com>
2021-10-20 02:14:17 +1100

    colorizer setup

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -1,10 +1,18 @@
+-- disable unused builtin vim plugins
 require "disable_builtin"
 
-require "plugins.packer_init"
+require "plugin_list"
+-- load configs
 require "plugins.telescope"
 require "plugins.lsp"
 require "plugins.treesitter"
 require "plugins.autopairs"
 require "plugins.statusline"
-
-require'nvim_comment'.setup {}
+require 'nvim_comment'.setup {}
+require 'colorizer'.setup {
+  'css',
+  'javascript',
+  html = {
+    'foreground',
+  }
+}
