5a904bbbed915a72bec8265b93cca20a71b793e8
Brian Aung <soekhantaung16@gmail.com>
2022-05-21 01:15:56 +1000

    colorscheme and some ui change

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,24 @@
 require("plugins")
 require("disable_builtin")
 
+-- my leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.g.snippets = "luasnip"
-
+-- manually specify files to load
 vim.cmd [[
 runtime! lua/core/options.lua
 runtime! lua/core/mappings.lua
 ]]
 
---vim.cmd [[ colorscheme kanagawa ]]
+-- load colorscheme
+require('kanagawa').setup({
+    commentStyle = "NONE",
+})
+vim.cmd [[ 
+set termguicolors
+set bg=dark
+colo kanagawa
+set list
+set listchars=tab:→\ ,eol:↲,extends:›,precedes:‹,nbsp:␣,trail:~
+]]
