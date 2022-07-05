6c9197902f7470251cf178840c8131ff41a601aa
Brian Aung <soekhantaung16@gmail.com>
2022-07-06 03:04:37 +1000

    fix install script to autoreload terminal during execution

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+
     sudo apt-get update && sudo apt-get upgrade -y
+
     # build essential and tools
+    sudo apt-get install curl
     sudo apt-get install build-essential manpages-dev -y
     sudo apt-get install valgrind
     sudo apt-get install xz-utils -y
+
+    # c language server
     sudo apt-get install clangd-12
+    tput reset && . ~/.bashrc
     sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 
     # install and source nix
     sh <(curl -L https://nixos.org/nix/install) --no-daemon
-    source ${HOME}/.bashrc
+    tput reset && . ~/.bashrc
 
     # install nix packages
     nix-env -iA \
@@ -21,6 +27,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         nixpkgs.stow \
         nixpkgs.fzf \
         nixpkgs.ripgrep \
+    tput reset && . ~/.bashrc
 
     # create symlink for all the dotfiles using stow
     stow zsh
@@ -28,18 +35,20 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     stow nvim
     stow git
     stow tmux
+    tput reset && . ~/.bashrc
 
     sudo chsh -s $(which zsh) $USER
+    tput reset && . ~/.zshrc
 
     # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
     # install node using version manager
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
-    source ${HOME}/.zshrc
+    tput reset && . ~/.zshrc
     nvm install node
 
     # install language servers for webdev
-    source ${HOME}/.zshrc
+    tput reset && . ~/.zshrc
     npm i -g vscode-langservers-extracted
     npm i -g typescript typescript-language-server
     npm i -g emmet-ls
