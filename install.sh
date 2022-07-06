91cf65a923a995110f641a427cefd2a8c5a70c5a
Brian Aung <soekhantaung16@gmail.com>
2022-07-06 20:53:46 +1000

    update install script

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "setup environment (linux)"
   
   sudo apt-get update && sudo apt-get upgrade -y
-  sudo apt-get install curl
+  sudo apt-get install curl -y
   sudo apt-get install build-essential manpages-dev -y
-  sudo apt-get install valgrind
+  sudo apt-get install valgrind -y
   sudo apt-get install xz-utils -y
 
   # c language server
-  sudo apt-get install clangd-12
+  sudo apt-get install clangd-12 -y
   tput reset
   sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 
@@ -28,15 +28,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     nixpkgs.ripgrep \
     nixpkgs.exa \
     nixpkgs.tmux
-  tput reset
   
   # link config files
   stow zsh
   stow starship
-  stow nvim
+  #stow nvim
   stow git
   stow tmux
-  tput reset
 
   # change shell to zsh
   if grep -q zsh /etc/shells; then
@@ -46,16 +44,14 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     command -v zsh | sudo tee -a /etc/shells
   fi
   chsh -s $(which zsh)
-  tput reset
 
   # install node
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
-  tput reset
   nvm install node
 
-  # install node packages
-  tput reset && . ~/.zshrc
-  npm i -g vscode-langservers-extracted
-  npm i -g typescript typescript-language-server
-  npm i -g emmet-ls
+  nvm exec npm i -g vscode-langservers-extracted
+  nvm exec npm i -g typescript typescript-language-server
+  nvm exec npm i -g emmet-ls
+
+  exec sudo --login --user $USER
 fi
