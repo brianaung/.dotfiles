c001650b03b8ecbe639d620e035f7917dbaa81b8
Brian Aung <soekhantaung16@gmail.com>
2022-07-07 00:37:04 +1000

    lalala

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,18 +9,6 @@ vim.g.maplocalleader = " "
 vim.cmd [[
 runtime! lua/core/options.lua
 runtime! lua/core/mappings.lua
-runtime! lua/plugin/emmet.vim
-]]
-
--- load colorscheme
-vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_background = 'hard'
-vim.g.gruvbox_material_disable_italic_comment = 1
-vim.g.gruvbox_material_visual = 'reverse'
-vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
-vim.g.gruvbox_material_statusline_style = 'mix'
-vim.cmd [[
-set termguicolors
-set bg=dark
-colo gruvbox-material
+runtime! lua/plugin/*.vim
+runtime! lua/plugin/*.lua
 ]]
