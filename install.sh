b12b2cbb97e96b9e4224c45a5cf8583f67d58f16
Brian Aung <76241135+brianaung@users.noreply.github.com>
2022-07-06 03:13:34 -0700

    make script run without having to reload shell manually

--- install.sh
+++ install.sh
@@ -1,55 +1,61 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "setup environment (linux)"
+  
+  sudo apt-get update && sudo apt-get upgrade -y
+  sudo apt-get install curl
+  sudo apt-get install build-essential manpages-dev -y
+  sudo apt-get install valgrind
+  sudo apt-get install xz-utils -y
 
-    sudo apt-get update && sudo apt-get upgrade -y
+  # c language server
+  sudo apt-get install clangd-12
+  tput reset
+  sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 
-    # build essential and tools
-    sudo apt-get install curl
-    sudo apt-get install build-essential manpages-dev -y
-    sudo apt-get install valgrind
-    sudo apt-get install xz-utils -y
+  # use nix package manager
+  sh <(curl -L https://nixos.org/nix/install) --no-daemon
+  . /home/$USER/.nix-profile/etc/profile.d/nix.sh
 
-    # c language server
-    sudo apt-get install clangd-12
-    tput reset && . ~/.bashrc
-    sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
+  # nix packages
+  nix-env -iA \
+    nixpkgs.neovim \
+    nixpkgs.zsh \
+    nixpkgs.starship \
+    nixpkgs.stow \
+    nixpkgs.fzf \
+    nixpkgs.ripgrep \
+    nixpkgs.exa \
+    nixpkgs.tmux
+  tput reset
+  
+  # link config files
+  stow zsh
+  stow starship
+  stow nvim
+  stow git
+  stow tmux
+  tput reset
 
-    # install and source nix
-    sh <(curl -L https://nixos.org/nix/install) --no-daemon
-    tput reset && . ~/.bashrc
+  # change shell to zsh
+  if grep -q zsh /etc/shells; then
+    echo "zsh already exists"
+  else
+    echo "adding zsh"
+    command -v zsh | sudo tee -a /etc/shells
+  fi
+  chsh -s $(which zsh)
+  tput reset
 
-    # install nix packages
-    nix-env -iA \
-        nixpkgs.neovim \
-        nixpkgs.zsh \
-        nixpkgs.starship \
-        nixpkgs.stow \
-        nixpkgs.fzf \
-        nixpkgs.ripgrep \
-    tput reset && . ~/.bashrc
+  # install node
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
+  tput reset
+  nvm install node
 
-    # create symlink for all the dotfiles using stow
-    stow zsh
-    stow starship
-    stow nvim
-    stow git
-    stow tmux
-    tput reset && . ~/.bashrc
-
-    sudo chsh -s $(which zsh) $USER
-    tput reset && . ~/.zshrc
-
-    # git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-    # install node using version manager
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
-    tput reset && . ~/.zshrc
-    nvm install node
-
-    # install language servers for webdev
-    tput reset && . ~/.zshrc
-    npm i -g vscode-langservers-extracted
-    npm i -g typescript typescript-language-server
-    npm i -g emmet-ls
+  # install node packages
+  tput reset && . ~/.zshrc
+  npm i -g vscode-langservers-extracted
+  npm i -g typescript typescript-language-server
+  npm i -g emmet-ls
 fi
