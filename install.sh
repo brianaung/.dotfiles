c0bb449efff4663c9be1e9b528925a8e7ee83435
Brian Aung <soekhantaung16@gmail.com>
2022-12-23 03:54:23 +1100

    update install script

--- install.sh
+++ install.sh
@@ -2,17 +2,13 @@
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "setup environment (linux)"
-  
-  sudo apt-get update && sudo apt-get upgrade -y
-  sudo apt-get install curl -y
-  sudo apt-get install build-essential manpages-dev -y
-  sudo apt-get install valgrind -y
-  sudo apt-get install xz-utils -y
 
-  # c language server
-  sudo apt-get install clangd-12 -y
-  tput reset
-  sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
+  sudo apt update && sudo apt upgrade -y
+
+  # c language server (i have escaped c)
+  # sudo apt-get install clangd-12 -y
+  # tput reset
+  # sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 
   # use nix package manager
   sh <(curl -L https://nixos.org/nix/install) --no-daemon
@@ -28,14 +24,18 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     nixpkgs.ripgrep \
     nixpkgs.exa \
     nixpkgs.tmux \
-    nixpkgs.ninja
-  
+
   # link config files
   stow zsh
   stow starship
   stow nvim
   stow git
   stow tmux
+  stow alacritty
+
+  # zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+  source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
   # change shell to zsh
   if grep -q zsh /etc/shells; then
@@ -47,18 +47,20 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   chsh -s $(which zsh)
 
   # install node
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | zsh
-  tput reset && . ~/.zshrc
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | zsh
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   nvm install node
 
-  nvm exec npm i -g vscode-langservers-extracted
-  nvm exec npm i -g typescript typescript-language-server
-  nvm exec npm i -g emmet-ls
+  npm i -g vscode-langservers-extracted
+  npm i -g typescript typescript-language-server
+  npm i -g @prisma/language-server
 
   # prettier lua
-  curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
-  . $HOME/.cargo/env
-  cargo install stylua
+  # curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+  # . $HOME/.cargo/env
+  # cargo install stylua
 
   exec sudo --login --user $USER
 fi
