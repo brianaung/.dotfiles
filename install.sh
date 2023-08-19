785921a57d81f08a7be015d4660c05aeea6474ac
Brian Aung <76241135+brianaung@users.noreply.github.com>
2023-08-20 01:22:46 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,36 +6,10 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt update && sudo apt upgrade -y
 
   # needed for some packages
-  sudo apt install build-essential
-
-  # install nix package manager
-  if ! command -v nix > /dev/null 2>&1; then
-    echo "Installing Nix package manager..."
-    sh <(curl -L https://nixos.org/nix/install) --daemon
-  else
-    echo "Nix is already installed!"
-  fi
-  source $HOME/.nix-profile/etc/profile.d/nix.sh
-
-  # install required packages using nix
-  nix-env -iA \
-    nixpkgs.stow \
-    nixpkgs.neovim \
-    nixpkgs.fish \
-    # nixpkgs.starship \
-    nixpkgs.fzf \
-    nixpkgs.ripgrep \
-    nixpkgs.exa \
-    nixpkgs.tmux \
-
-  # link config files
-  # stow starship
-  stow nvim
-  stow git
-  stow tmux
-  # stow alacritty
+  sudo apt install build-essential -y
 
   # change shell to fish
+  sudo apt-get install fish -y
   if grep -q fish /etc/shells; then
     echo "Fish shell already exists!"
   else
@@ -43,19 +17,40 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo $(which fish) | sudo tee -a /etc/shells
   fi
   chsh -s $(which fish)
+  
+########################################################################
 
-  # Install nvm (node version manager)
-  if ! command -v nvm > /dev/null 2>&1; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | zsh
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  # install nix package manager
+  if ! command -v nix > /dev/null 2>&1; then
+    echo "Installing Nix package manager..."
+    # sh <(curl -L https://nixos.org/nix/install) --no-daemon
+    curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
+    . $HOME/.nix-profile/etc/profile.d/nix.fish
   else
-    echo "NVM is already installed"
+    echo "Nix is already installed!"
   fi
 
+  # install required packages using nix
+  nix-env -iA \
+    nixpkgs.stow \
+    nixpkgs.neovim \
+    nixpkgs.starship \
+    nixpkgs.fzf \
+    nixpkgs.ripgrep \
+    nixpkgs.exa \
+    nixpkgs.tmux
+
+  # link config files
+  stow starship
+  stow nvim
+  stow git
+  stow tmux
+
+  curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+  fisher install jorgebucaran/nvm.fish
+
   # Install node and global packages
-  nvm install node
+  nvm install latest
   npm i -g yarn
   npm i -g vscode-langservers-extracted
   npm i -g typescript typescript-language-server
